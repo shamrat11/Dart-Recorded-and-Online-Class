7adece882487e847a0711761d676ad70f4bd9e15
Mehedi Hasan Shamrat <mhshamrat11@gmail.com>
2025-06-03 00:50:21 +0600

    Initial commit

---
+++ bin/git_and_github.dart
@@ -0,0 +1,5 @@
+import 'package:git_and_github/git_and_github.dart' as git_and_github;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_and_github.calculate()}!');
+}
---
+++ test/git_and_github_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_and_github/git_and_github.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
